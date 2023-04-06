b946c10d5e15609a336e4478e1458308485aeb3e
danikorv <korvd115@newschool.edu>
2023-04-06 10:39:41 -0400

    h

--- script.js
+++ script.js
@@ -10,14 +10,18 @@ fetch("items.json")
       out += `
          <div class="boot">
             <div class=""boot-image> <img src='${item.boots}'> </div>
-            <div class=“era”>${item.era}</div> 
-            
-            <div>${item.number}</div>
-            <div>${item.color}</div>
-            <div>${item.rating}</div>
+            <div class="era">${item.era}</div> 
+            <div class="number">${item.number}</div> 
+            <div class="color">${item.color}</div> 
+            <div class="rating">${item.rating}</div> 
+
+         
          </div>
 
       `;
+
+
+
    }
  
    placeholder.innerHTML = out;
